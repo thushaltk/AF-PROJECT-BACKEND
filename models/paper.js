aec07e56ba844716bcc3a49479f95bf3ff3dd9f2
kish_deemu <deemudk@gmail.com>
2021-07-01 00:46:28 +0530

    Completed editor stuff

--- models/paper.js
+++ models/paper.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const paperSchema = new mongoose.Schema({
+    id: {type: String, required: true},
     researchPaperTitle: {type: String, required: true},
     coverImgURL: {type: String, required: true},
     authorName: {type: String, required: true},
